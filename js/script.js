f8dbb75d5d6fc0f3f594dfdf916fb526e70e5abf
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 12:39:50 -0800

    Removed default vars and added details to prompt vars

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 //Guerra, Andre 12/13/14 Expressions - Industry
 
-var grid = 960;
-var columns = 16;
-var margins = 10;
-
-var gridSize = prompt("What size is your grid container? (write without px)");
-var numberofColumns = prompt("How many columns would you like?");
+var gridSize = prompt("What size is your grid container in pixels? i.e. 960, 1000, 1100, etc. (write without px)");
+var numberofColumns = prompt("How many columns would you like? i.e. 8, 10, 12");
 var marginSpace = prompt("How much space would you like inbetween your columns?");
+
+var columnSize = (gridSize - marginSpace) / numberofColumns;
