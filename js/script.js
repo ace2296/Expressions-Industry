8c42243f390d5a8d7123736628a29716e3617b00
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 12:28:55 -0800

    Prompt variable setup

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ var grid = 960;
 var columns = 16;
 var margins = 10;
 
+var gridSize = prompt("What size is your grid container? (write without px)");
+var numberofColumns = prompt("How many columns would you like?");
+var marginSpace = prompt("How much space would you like inbetween your columns?");
