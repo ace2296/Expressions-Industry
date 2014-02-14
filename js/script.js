012240228b7ef6375fc3d82292c88daff4621e49
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 16:46:27 -0800

    Added fullwidth column size

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@ var gridSize = prompt("What size is your grid container in pixels? i.e. 960, 100
 var numberofColumns = prompt("How many columns would you like? i.e. 8, 10, 12");
 var marginSpace = prompt("How much space would you like inbetween your columns?");
 
-var columnSize = (gridSize - marginSpace) / numberofColumns;
+var AllColumnSize = (gridSize - marginSpace) / numberofColumns;
+
+var fullWidth = (gridSize - marginSpace) + "px";
+
+
+alert("Total width of your grid is " + gridSize + "px and your column widths for " + numberofColumns + "are \n Fullwidth column:" + fullWidth + "px \n" );
