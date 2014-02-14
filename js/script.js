3a2a877e79bbcdf2715e89a3a3ec3761eac25a46
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 17:14:09 -0800

    Inserted var grid size note

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Guerra, Andre 12/13/14 Expressions - Industry
 
-var gridSize = prompt("What size is your grid container in pixels? i.e. 960, 1000, 1100, etc. (write without px)");
+var gridSize = prompt("What size is your grid container in pixels? i.e. 960, 1000, 1100, etc. (write without px)"); //Create a base to perform the column width calculation
 var numberofColumns = prompt("How many columns would you like? i.e. 8, 10, 12");
 var padding = prompt("How much padding outside each column?");
 
