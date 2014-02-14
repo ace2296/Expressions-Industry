29c56cb825fceaa381e0710a318d527ab94468c9
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 17:23:01 -0800

    HTMl character

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Guerra, Andre 12/13/14 Expressions - Industry
 
 var gridSize = prompt("What size is your grid container in pixels? i.e. 960, 1000, 1100, etc. (write without px)"); //Create a base to perform the column width calculation
-var numberofColumns = prompt("How many columns would you like? i.e. 8, 10, 12"); //Creates the number of columns the grid contains
+var numberofColumns = prompt("How many columns would you like? (insert up to 6)"); //Creates the number of columns the grid contains
 var padding = prompt("How much padding outside each column?"); //Creates the space inbetween the gride columns
 
 padding == numberofColumns * 2 //Calculates the total padding pixels
