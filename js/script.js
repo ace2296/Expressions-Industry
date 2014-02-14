b27003daf029dd9e00a4c004a9cf5bac06bac04a
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 17:15:02 -0800

    Inserted var padding note

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 var gridSize = prompt("What size is your grid container in pixels? i.e. 960, 1000, 1100, etc. (write without px)"); //Create a base to perform the column width calculation
 var numberofColumns = prompt("How many columns would you like? i.e. 8, 10, 12"); //Creates the number of columns the grid contains
-var padding = prompt("How much padding outside each column?");
+var padding = prompt("How much padding outside each column?"); //Creates the space inbetween the gride columns
 
 padding == numberofColumns * 2
 
