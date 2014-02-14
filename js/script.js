b1b2477ab147c1e550e68c4de38ab3b288c9218a
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 17:03:08 -0800

    Added column widths

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ var padding = prompt("How much padding outside each column?");
 
 padding *= 2
 
-var fullWidth = (gridSize -padding) + "px";
+var fullWidth = (gridSize - padding);
+var oneHalf = fullWidth / 2;
+var oneThird = fullWidth / 3;
+var oneFourth = fullWidth / 4;
+var oneFifth = fullWidth / 5;
+var oneSixth = fullWidth / 6;
 
 
 alert("Total width of your grid is " + gridSize + "px and your column widths for " + numberofColumns + "are \n Fullwidth:" + fullWidth + "px \n" );
